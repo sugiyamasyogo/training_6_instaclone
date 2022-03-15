ef8019de2e89d32a4dadba83b402acfa061c96c7
shogo sugiyama <terugo72@gmail.com>
2022-03-15 13:52:58 +0900

    firebase setting

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:training_6_instaclone/screens/home_screen.dart';
 import 'package:training_6_instaclone/style.dart';
 
+import 'firebase_options.dart';
 import 'generated/l10n.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -27,11 +33,11 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
           brightness: Brightness.dark,
           elevatedButtonTheme: ElevatedButtonThemeData(
-              style: ElevatedButton.styleFrom(primary: Colors.white),
+            style: ElevatedButton.styleFrom(primary: Colors.white),
           ),
           iconTheme: IconThemeData(color: Colors.white),
           fontFamily: RegularFont),
-       home:  HomeScreen(),
+      home: HomeScreen(),
     );
   }
 }
